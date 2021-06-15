4c6527fd5a1a362a0793dd93a5e2ae9e3aad0e3c
Reinold Rivera <oknolaz.freedom@protonmail.com>
2021-06-15 17:17:27 +0300

    v0.7.0

--- core/recognise.py
+++
@@ -1,6 +0,0 @@
-from . import speak
-import plyer
-
-def recognise(widget):
-    say = ''
-    return say
--- core/speak.py
+++
@@ -1,8 +0,0 @@
-import plyer
-from kivymd.uix.label import MDLabel
-
-
-def speak(string, widget):
-    plyer.tts.speak(string)
-    printtext = MDLabel(text=string)
-    widget.add_widget(printtext)
