1e7f22bfeb773cdd397d14bfc9422000b8f43142
okno <oknolaz.freedom@protonmail.com>
2022-02-01 18:27:54 +0300

    Now time and date are called by separate triggers. Fixed a bug with launching cyclic skills. Returned old messages.

--- vasisualy/core/speak.py
+++ vasisualy/core/speak.py
@@ -4,32 +4,8 @@ from kivymd.uix.label import MDLabel
 
 def speak(string, widget):
     plyer.tts.speak(string)
-    if len(string) > 86:
-        printtext = MDLabel(
-            text=string[:86],
-            size_hint_x=1
-        )
-        widget.add_widget(printtext)
-        last = 86
-        while last:
-            if len(string) <= last+86:
-                printtext = MDLabel(
-                    text=string[last:],
-                    size_hint_x=1
-                )
-                widget.add_widget(printtext)
-                last = 0
-            else:
-                printtext = MDLabel(
-                    text=string[last:last+86],
-                    size_hint_x=1
-                )
-                widget.add_widget(printtext)
-                last += 86
-    else:
-        printtext = MDLabel(
-            text=string,
-            size_hint_x=1
-        )
-        widget.add_widget(printtext)
-
+    printtext = MDLabel(
+        text=string,
+        size_hint_x=1
+    )
+    widget.add_widget(printtext)
